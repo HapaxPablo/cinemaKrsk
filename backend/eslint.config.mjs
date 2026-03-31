e04f44be279ffbbb038a4b87ce1ee8611ffd5ed4
zabor <pahapasha11@gmail.com>
2026-03-31 17:26:58 +0700

    feat:Инициализация бэкенда с использованием NestJS. - Добавление файла package.json с зависимостями и скриптами для сборки, тестирования и проверки кода. - Создание файла tsconfig.json для конфигурации TypeScript со строгими настройками и разрешением модулей. - Добавление файла tsconfig.build.json для исключения определенных каталогов и файлов в процессе сборки.

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,54 @@
+// @ts-check
+import eslint from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+
+  {
+    files: ['**/*.dto.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-call': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.entity.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+    },
+  }
+)
